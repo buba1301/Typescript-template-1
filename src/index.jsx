a732967d4627e4391494e9f3f790b46a599b37a8
Boris Muzalevskii <52819672+buba1301@users.noreply.github.com>
2022-12-21 21:36:29 +0300

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,9 @@
 import ReactDOM from 'react-dom';
-import { Provider } from "react-redux";
-
-import store from 'store';
 
 import App from './app';
 
 import './globalStyles';
 
-ReactDOM.render(
-  <Provider store={ store }>
-    <App />
-  </Provider>,
-  document.getElementById('app')
-);
+const container = document.getElementById('app');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<App />);
